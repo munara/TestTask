68266c18fd82cb17d43f79f65f70eb8a7c553637
Munara Sultanova <munara.sultanova@sibers.com>
2020-05-05 13:39:18 +0600

    Initial Commit

---
+++ RSSFeedReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSFeedReader
+//
+//  Created by Munara on 5/5/20.
+//  Copyright © 2020 Munara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
